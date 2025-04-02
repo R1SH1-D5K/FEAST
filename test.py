28c77aa297d94cda33e4490e9ea3b942762f0268
R1SH1-D5K <rishidsk@gmail.com>
2025-04-02 12:30:01 +0530

    Remove all files

--- test.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-mongo_uri = "mongodb://localhost:27017/"
-client = MongoClient(mongo_uri)
-db = client["recipeDB"]
-collection = db["recipes"]
-
-recipe = db.recipes.find_one()
-print(recipe)
